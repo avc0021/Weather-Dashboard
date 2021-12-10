cab09559a233f8afd62a5671b312807ff9c14c50
avc0021 <avc0021@gmail.com>
2021-12-09 21:28:45 -0600

    comment out js

---
+++ assets/js/script.js
@@ -0,0 +1,51 @@
+// <!DOCTYPE html>
+// <html>
+//   <head>
+//     <meta charset="utf-8">
+//     <meta name="viewport" content="width=device-width, initial-scale=1">
+//     <title>Weather Dashboard</title>
+//     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
+//     <link rel="stylesheet" href="./assets/css/style.css" />
+// </head>
+//   <body>
+
+//     <!--title-->
+//     <header class="has-text-centered">
+//       <h1 class="wd is-size-2">
+//         Weather Dashboard
+//       </h1>
+//     </header>
+
+// </main>
+//     <!--search by city-->
+//     <section class="section">
+//         <div class="container">
+//         <div class="search is-pulled-left">Search for a city:</div>
+//             <input type="text" id="name" name="name" required
+//             minlength="4" maxlength="8" size="10">    
+//             <button type="button" class="button is-info ">Search</button>
+
+//             <div class="cities">
+//             <button class="austin">Austin</button>
+//             <button class="chicago">Chicago</button>
+//             <button class="new-york">New York</button>
+//             <button class="las-vegas">Las Vegas</button>
+//         </div>
+//         </div>
+//     </section>
+
+//     <section class="section">
+//   <div class="table-container is-pulled-right">
+//     <table class="current-weather">
+//      Current Weather/Date goes here
+//     </table>
+//   </div>
+//   </section>
+  
+//   <div class="five-day-title">
+//     <h2>5 day forcast</h2>
+//     <table class="5-day-forecast">
+//      Listing for 5 day forcast
+//     </table>
+//   </div>
+// </main>?????posible use the car class in each section?
