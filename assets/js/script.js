530dbd0fc253e3befe4e6c0b170a0f785f02fbdf
avc0021 <avc0021@gmail.com>
2021-12-11 12:10:48 -0600

    added fetch function for preset buttons and current city

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,42 @@
 //variables for project
 var search = document.getElementById (search)
 
-//call web api
-var getCurrentWeather = function() {
-    fetch("https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=d310cdc3e7de424fc0047cf1fd72fd27");
-    console.log("function called!")
+//call web api display current location details and todays day
+var getSaWeather = function() {
+    fetch("https://api.openweathermap.org/data/2.5/onecall?lat=29.42&lon=-99.49&appid=d310cdc3e7de424fc0047cf1fd72fd27");
+    console.log("function called SA!")
 };
 
-getCurrentWeather();
+getSaWeather();
 
 //parse info/ pull only info needed
 
 //click austin button and displays weather and 5 day forcast
+var getAustinWeather = function() {
+    fetch("https://api.openweathermap.org/data/2.5/onecall?lat=30.26&lon=-97.73&appid=d310cdc3e7de424fc0047cf1fd72fd27");
+    console.log("function called austin!")
+};
+getAustinWeather();
 
 //click chicago button and displays weather and 5 day forcast
+var getChicagoWeather = function() {
+    fetch("https://api.openweathermap.org/data/2.5/onecall?lat=41.88&lon=-87.62&appid=d310cdc3e7de424fc0047cf1fd72fd27");
+    console.log("function called chicago!")
+};
+getChicagoWeather();
 
 //click new york button and displays weather and 5 day forcast
+var getNewYorkWeather = function() {
+    fetch("https://api.openweathermap.org/data/2.5/onecall?lat=40.73&lon=-73.93&appid=d310cdc3e7de424fc0047cf1fd72fd27");
+    console.log("function called new york!")
+};
+getNewYorkWeather();
 
 //click las vegas button and displays weather and 5 day forcast
+var getVegasWeather = function() {
+    fetch("https://api.openweathermap.org/data/2.5/onecall?lat=36.11&lon=-115.17&appid=d310cdc3e7de424fc0047cf1fd72fd27");
+    console.log("vegas!")
+};
+getVegasWeather();
 
 //click button and displays weather and 5 day forcast for city requested
